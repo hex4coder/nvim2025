1f7f02293ec2a4d1371a53ca630a1b94ac05bfad
hex4coder <the.programmer.luyo@gmail.com>
2025-03-19 09:31:12 +0800

    My new neovim configs

---
+++ lua/plugins/nvim-tree.lua
@@ -0,0 +1,15 @@
+return {
+   'nvim-tree/nvim-tree.lua',
+   dependencies = {
+        'nvim-tree/nvim-web-devicons'
+   },
+   config = function()
+        require("nvim-tree").setup({})
+
+        -- toggle file tree
+        vim.keymap.set("n", "<C-n>", "<cmd> NvimTreeToggle <CR>", {})
+
+        -- focus
+        vim.keymap.set("n", "<leader>e", "<cmd> NvimTreeFocus <CR>", { noremap = true, silent = true })
+   end
+}
