abcd398ac6b519ddda41a51252a749ce3c4e18cd
hex4coder <the.programmer.luyo@gmail.com>
2025-04-06 19:29:39 +0800

    Add desc to lsp actions

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -99,26 +99,54 @@ return {
 
 			local opts = { noremap = true, silent = true }
 
+            opts.desc = "Goto defenition code"
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+
+            opts.desc = "Goto declaration code"
 			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+
+            opts.desc = "Goto implementation code"
 			vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
 
             -- code actions
+            opts.desc = "Run code action in code"
             vim.keymap.set("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
 
+            opts.desc = "Hover to code"
+            vim.keymap.set("n", 'K', vim.lsp.hover, opts)
+
+            opts.desc = "Goto prev docs code"
 			vim.keymap.set("n", "<C-p>", vim.diagnostic.goto_prev, opts)
+            opts.desc = "Goto next docs code"
 			vim.keymap.set("n", "<A-p>", vim.diagnostic.goto_next, opts)
+
+            opts.desc = "Rename a variable"
 			vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, opts)
 
+            opts.desc = "Open signature help"
 			vim.keymap.set("n", "<C-K>", vim.lsp.buf.signature_help, opts)
+
+            opts.desc = "Add workspace folder"
 			vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts)
+
+            opts.desc = "Remove workspace folder"
 			vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts)
+
+            opts.desc = "List workspace folders"
 			vim.keymap.set("n", "<leader>wl", function()
 				print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
 			end, opts)
+
+            opts.desc = "Goto type defenition"
 			vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts)
+
+            opts.desc = "References in code"
 			vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+
+            opts.desc = "Open diagnostik in float"
 			vim.keymap.set("n", "<leader>E", vim.diagnostic.open_float, opts)
+
+            opts.desc = "Set location diagnostic"
 			vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, opts)
 		end,
 	},
